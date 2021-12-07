19900b2582d529e74309fbf6d5d41692a16b3fa8
Mark Bucciarelli <mkbucc@gmail.com>
2021-12-07 07:28:01 -0500

    Delete two unused classes

--- src/com.acsredux.user/com/acsredux/auth/services/InsertedNewUser.java
+++
@@ -1,6 +0,0 @@
-package com.acsredux.user.services;
-
-import com.acsredux.user.commands.AddUser;
-import java.time.Instant;
-
-public record InsertedNewUser(AddUser command, long newUserID, Instant savedOn) {}
