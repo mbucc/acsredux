e7c2682b57ef54d431c530029b44df17544f0cb4
Mark Bucciarelli <mkbucc@gmail.com>
2022-01-29 16:39:06 -0500

    Remove temporary directory after building docs
    Improve description of why we go through the trouble of
    creating a temporary directory when generating javadocs.

--- scripts/javadoc.sh
+++ scripts/javadoc.sh
@@ -1,15 +1,27 @@
 # !/bin/sh -e
 # January 16, 2022
-# Avoid errors Javadoc prints for test files (even when they are not in sourcefiles).
 
 D=tmp/src
-
-
 rm -rf tmp
 mkdir -p $D
 
+#
+#		javadoc prints errors for test files even
+#		when they are excluded from the list of
+#		sourcefiles passed to the command.
+#
+#		So we use a temporary directory and
+#		"manually" remove the test files.
+#
 cp -r src/ $D
 find $D -type f | egrep '(/Test|/Mock|/ManualTest)' | xargs rm
 rm -rf $D/com.acsredux.lib.testutil
 
-javadoc -quiet -Xdoclint:none  -p lib:testlib -d docs --module-source-path $D $(find $D -name '*.java')
+javadoc \
+   -quiet \
+   -Xdoclint:none  \
+   -p lib:testlib \
+   -d docs \
+   --module-source-path $D $(find $D -name '*.java')
+
+rm -rf tmp
