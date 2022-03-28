30ad5c676dd0b620b8d842b34d6884aa815a71da
Mark Bucciarelli <mkbucc@gmail.com>
2022-03-28 06:41:10 -0400

    Make stop.sh work if previous terminal started server
    e.g. if i started in intellij terminal, then closed intellij.

--- stop.sh
+++ stop.sh
@@ -1,4 +1,4 @@
 #! /bin/sh -e
 
-pid=$(ps -o pid,args|grep java|grep -v grep|awk '{print $1}')
+pid=$(ps -a -o pid,args|grep 'java.*com.acsredux.adapter.web'|grep -v grep|awk '{print $1}')
 [ "x$pid" != "x" ] && kill $pid || echo already stopped.
