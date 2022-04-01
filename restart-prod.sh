2712ac18d95e0c08e2a64ac982c7395a22a84da4
Mark Bucciarelli <mkbucc@gmail.com>
2022-04-01 12:46:40 -0400

    Add script to start server with file system adapter
    To run Cypress tests again adapter used in production.

---
+++ restart-prod.sh
@@ -0,0 +1,17 @@
+#! /bin/sh -e
+
+if [ ! -f "siteinfo.json" ]; then
+ cat > siteinfo.json << EOF
+{
+  "siteTitle": "A Test Site",
+  "siteDescription": "foobar",
+  "siteEmail": "t@t.com",
+  "analyticsScriptTag": ""
+}
+EOF
+fi
+
+sh ./stop.sh
+make clean
+make SKIP_FMT=Y
+IS_PRODUCTION=Y sh ./start.sh
