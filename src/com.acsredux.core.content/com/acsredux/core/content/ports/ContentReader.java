22a8b70683a023e951daf6445d92cc9dfad99847
Mark Bucciarelli <mkbucc@gmail.com>
2022-04-29 16:28:30 -0400

    Add default implementation of findByID :: ContentID -> Content

--- src/com.acsredux.core.content/com/acsredux/core/content/ports/ContentReader.java
+++ src/com.acsredux.core.content/com/acsredux/core/content/ports/ContentReader.java
@@ -4,9 +4,19 @@ import com.acsredux.core.base.MemberID;
 import com.acsredux.core.content.entities.Content;
 import com.acsredux.core.content.values.ContentID;
 import java.util.List;
+import java.util.Optional;
 
 public interface ContentReader {
   Content getByID(ContentID x);
+
+  default Optional<Content> findByID(ContentID x) {
+    try {
+      return Optional.of(getByID(x));
+    } catch (Exception e) {
+      return Optional.empty();
+    }
+  }
+
   List<Content> findByMemberID(MemberID x);
   List<Content> findChildrenOfID(ContentID x);
 }
